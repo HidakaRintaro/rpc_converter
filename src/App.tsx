8c82e454e4ba07facddf262ec560c37987a50d2e
HidakaRintaro <53255166+HidakaRintaro@users.noreply.github.com>
2021-10-21 00:11:54 +0900

    レイアウトを整えた

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,75 @@
+import { ChangeEvent, useState } from "react";
 import { ArrowDownIcon, ArrowUpIcon } from "@chakra-ui/icons";
 import {
-  Box,
   Button,
+  ButtonGroup,
+  Center,
   ChakraProvider,
+  Container,
+  FormControl,
+  FormHelperText,
+  FormLabel,
   Heading,
-  HStack,
+  Text,
   Textarea,
 } from "@chakra-ui/react";
 import "./App.css";
 
 export const App = () => {
+  const [rpcValue, setRpcValue] = useState("");
+  // const onChangeCheckText = (e: ChangeEvent<HTMLTextAreaElement>) => {
+  //   const s = e.target.value;
+  //   while (s.match(/[^A-Z^a-z\d\-]/)) {
+  //     setRpcValue(s.replace(/[^A-Z^a-z\d\-]/, ""));
+  //   }
+  //   console.log(s);
+  // };
+
   return (
     <ChakraProvider>
-      <Box w="50%" mx="auto">
-        <Heading as="h1">RPC CONVERTER</Heading>
-        <Textarea placeholder="test" />
-        <HStack>
-          <Button
-            leftIcon={<ArrowDownIcon />}
-            colorScheme="teal"
-            variant="outline"
-          >
-            逆ポーランド記法
-          </Button>
-          <Button
-            leftIcon={<ArrowUpIcon />}
-            colorScheme="teal"
-            variant="outline"
-          >
-            中置記法
-          </Button>
-        </HStack>
-        <Textarea placeholder="test" />
-      </Box>
+      <Container mt="3">
+        <Heading as="h1" mb="3">
+          逆ポーランド記法 変換器
+        </Heading>
+        <Text color="gray.600" mb="6">
+          一般的な数式(中置記法)と逆ポーランド記法の相互変換器です。
+        </Text>
+        <FormControl>
+          <FormLabel>一般的な数式(中置記法)</FormLabel>
+          <FormHelperText>
+            数字(0~9)、演算子(+-*/)、丸括弧、変数(英数字)を使用可能です。
+            <br />
+            全て半角文字で入力してください。
+          </FormHelperText>
+          <Textarea
+            mb="5"
+            placeholder="a + b - c"
+            // value={rpcValue}
+            // onChange={onChangeCheckText}
+          />
+        </FormControl>
+        <Center>
+          <ButtonGroup variant="outline" mb="5">
+            <Button leftIcon={<ArrowDownIcon />} colorScheme="teal">
+              逆ポーランド記法
+            </Button>
+            <Button leftIcon={<ArrowUpIcon />} colorScheme="teal">
+              中置記法
+            </Button>
+          </ButtonGroup>
+        </Center>
+        <FormControl>
+          <FormLabel>逆ポーランド記法</FormLabel>
+          <FormHelperText>
+            数字(0~9)、演算子(+-*/)、丸括弧を使用可能です。要素間は空白を入れてください。
+            <br />
+            全て半角文字で入力してください。
+          </FormHelperText>
+          <Textarea placeholder="a b + c -" />
+        </FormControl>
+      </Container>
     </ChakraProvider>
   );
 };
+
+// const RpcConvert = (formula: string) => {};
